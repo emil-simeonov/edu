ba9fc6466de3f3378fdf574026871fb72ef4083e
emil.simeonov <emil.simeonov@bulpros.com>
2016-01-10 14:48:20 +0200

    Commented echo server

--- tu-sofia/modern-java-technologies/vert.x/vertx-examples/src/main/java/bg/tusofia/vertx/EchoNonBlockingTCPServer.java
+++ tu-sofia/modern-java-technologies/vert.x/vertx-examples/src/main/java/bg/tusofia/vertx/EchoNonBlockingTCPServer.java
@@ -1,16 +1,26 @@
 package bg.tusofia.vertx;
 
 import bg.tusofia.tcp.TCPConstants;
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.streams.Pump;
 
-public class EchoNonBlockingTCPServer {
-    public static void main(String[] argv) {
+// We extend the abstract verticle, so we could add this service to a message bus, if necessary.
+public class EchoNonBlockingTCPServer extends AbstractVerticle {
+    @Override
+    public void start() throws Exception {
+        // Creates a non-blocking TCP server which registers a handler for incoming connections (anonymous Java function)
         Vertx.vertx().createNetServer().connectHandler(
                 sock -> {
-                    // Create a pump
+                    System.out.println("Processing incoming connection. Pumping out everything...");
+                    // Create and start a pump. Instances of this class read items from a {@link ReadStream} and write them to a {@link WriteStream}.
                     Pump.pump(sock, sock).start();
                 }
-        ).listen(TCPConstants.DEFAULT_PORT);
+        ).listen(TCPConstants.DEFAULT_PORT); // binds the server to a given server socket (channel)
+        System.out.println("Simple Echo non-blocking TCP server is started.");
+    }
+
+    public static void main(String[] argv) throws Exception {
+        new EchoNonBlockingTCPServer().start();
     }
 }
